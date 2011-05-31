2784a8d82fa3e6083825aded1e9ae1e2700155d7
psigen <psigen@gmail.com>
2011-05-31 21:58:38 +0000

    Added boilerplate code for handling how sensors are implemented.

--- client/trunk/src/edu/cmu/ri/airboat/interfaces/AirboatSensor.java
+++ client/trunk/src/edu/cmu/ri/airboat/interfaces/AirboatSensor.java
@@ -12,4 +12,9 @@ public interface AirboatSensor {
 
 	// Get 3D raw gyro readings
 	public double[] getGyro();
+        
+        public String[] getSensorTypes();
+        public String getSensorType(int port);
+        public boolean setSensorType(int port, String type);
+        public double[] getSensor(int port);
 }
