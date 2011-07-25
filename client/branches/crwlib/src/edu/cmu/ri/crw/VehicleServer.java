bd405cf53ab08780f73a553d237cf92be9eed875
psigen <psigen@gmail.com>
2011-07-25 10:55:06 +0000

    Added pub/sub implementation of one-way calls, added TODOs to vehicle server to indicate what is covered by ROS so far.

--- client/branches/crwlib/src/edu/cmu/ri/crw/VehicleServer.java
+++ client/branches/crwlib/src/edu/cmu/ri/crw/VehicleServer.java
@@ -15,34 +15,34 @@ public interface VehicleServer {
 	public void addStateListener(VehicleStateListener l);
 	public void removeStateListener(VehicleStateListener l);
 	public void setState(UtmPose state);
-	public UtmPoseWithCovarianceStamped getState();
+	public UtmPoseWithCovarianceStamped getState(); // TODO: add service call
 	
 	public void addImageListener(VehicleImageListener l);
 	public void removeImageListener(VehicleImageListener l);
 	public void startCamera(long numFrames, double interval, int width, int height, ImagingObserver obs);
 	public void stopCamera();
-	public CompressedImage captureImage(int width, int height);
+	public CompressedImage captureImage(int width, int height); // TODO: add service call
+	public CameraState getCameraStatus(); // TODO: add service call
 	
 	public void addSensorListener(int channel, VehicleSensorListener l);
 	public void removeSensorListener(int channel, VehicleSensorListener l);
-	public void setSensorType(int channel, SensorType type);
-	public SensorType getSensorType(int channel);
-	public int getNumSensors();
+	public void setSensorType(int channel, SensorType type); // TODO: add service call
+	public SensorType getSensorType(int channel); // TODO: add service call
+	public int getNumSensors(); // TODO: add service call
 	
 	public void addVelocityListener(VehicleVelocityListener l);
 	public void removeVelocityListener(VehicleVelocityListener l);
 	public void setVelocity(Twist velocity);
-	public TwistWithCovarianceStamped getVelocity();
+	public TwistWithCovarianceStamped getVelocity(); // TODO: add service call
 	
-	public boolean isAutonomous();
-	public void setAutonomous(boolean auto);
+	public boolean isAutonomous(); // TODO: add service call
+	public void setAutonomous(boolean auto); // TODO: add service call
 	
 	public void startWaypoint(UtmPose waypoint, WaypointObserver obs);
 	public void stopWaypoint();
-	public UtmPose getWaypoint();
-	public WaypointState getWaypointStatus();
+	public UtmPose getWaypoint(); // TODO: add service call
+	public WaypointState getWaypointStatus(); // TODO: add service call
 	
-	public void setPID(int axis, double[] gains);
-	public double[] getPID(int axis);
-	public CameraState getCameraStatus();
+	public void setPID(int axis, double[] gains); // TODO: add service call
+	public double[] getPID(int axis); // TODO: add service call
 }
