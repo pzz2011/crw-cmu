c67652b51bf32cd4889538287bc7db0dd3751239
psigen@gmail.com <psigen@gmail.com@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2011-07-25 13:58:42 +0000

    Updated proxy to provide all the service functionality.  Most functions should work now, I think.

--- client/branches/crwlib/src/edu/cmu/ri/crw/VehicleServer.java
+++ client/branches/crwlib/src/edu/cmu/ri/crw/VehicleServer.java
@@ -8,41 +8,41 @@ import org.ros.message.sensor_msgs.CompressedImage;
 
 public interface VehicleServer {
 	
-	public enum SensorType { ANALOG, DIGITAL, TE };
-	public enum WaypointState { GOING, DONE };
-	public enum CameraState { CAPTURING, OFF };
+	public enum SensorType { ANALOG, DIGITAL, TE, UNKNOWN };
+	public enum WaypointState { GOING, DONE, UNKNOWN };
+	public enum CameraState { CAPTURING, OFF, UNKNOWN };
 
 	public void addStateListener(VehicleStateListener l);
 	public void removeStateListener(VehicleStateListener l);
-	public void setState(UtmPose state); // TODO: add service call '
-	public UtmPoseWithCovarianceStamped getState(); // TODO: add service call '
+	public void setState(UtmPose state);
+	public UtmPoseWithCovarianceStamped getState();
 	
 	public void addImageListener(VehicleImageListener l);
 	public void removeImageListener(VehicleImageListener l);
 	public void startCamera(long numFrames, double interval, int width, int height, ImagingObserver obs);
 	public void stopCamera();
-	public CompressedImage captureImage(int width, int height); // TODO: add service call '
-	public CameraState getCameraStatus(); // TODO: add service call '
+	public CompressedImage captureImage(int width, int height);
+	public CameraState getCameraStatus();
 	
 	public void addSensorListener(int channel, VehicleSensorListener l);
 	public void removeSensorListener(int channel, VehicleSensorListener l);
-	public void setSensorType(int channel, SensorType type); // TODO: add service call '
-	public SensorType getSensorType(int channel); // TODO: add service call '
-	public int getNumSensors(); // TODO: add service call '
+	public void setSensorType(int channel, SensorType type);
+	public SensorType getSensorType(int channel);
+	public int getNumSensors();
 	
 	public void addVelocityListener(VehicleVelocityListener l);
 	public void removeVelocityListener(VehicleVelocityListener l);
 	public void setVelocity(Twist velocity);
-	public TwistWithCovarianceStamped getVelocity(); // TODO: add service call '
+	public TwistWithCovarianceStamped getVelocity();
 	
-	public boolean isAutonomous(); // TODO: add service call '
-	public void setAutonomous(boolean auto); // TODO: add service call '
+	public boolean isAutonomous();
+	public void setAutonomous(boolean auto);
 	
 	public void startWaypoint(UtmPose waypoint, WaypointObserver obs);
 	public void stopWaypoint();
-	public UtmPose getWaypoint(); // TODO: add service call '
-	public WaypointState getWaypointStatus(); // TODO: add service call '
+	public UtmPose getWaypoint();
+	public WaypointState getWaypointStatus();
 	
-	public void setPID(int axis, double[] gains); // TODO: add service call '
-	public double[] getPID(int axis); // TODO: add service call '
+	public void setPID(int axis, double[] gains);
+	public double[] getPID(int axis);
 }
