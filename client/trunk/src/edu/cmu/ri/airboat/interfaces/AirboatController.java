6f3cafd37f26c569f9e4701a3e8d3be55a773976
psigen@gmail.com <psigen@gmail.com@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2011-07-26 17:07:11 +0000

    Reverted modifications to AirboatController (those changes go into the crwlib branch).

--- client/trunk/src/edu/cmu/ri/airboat/interfaces/AirboatController.java
+++ client/trunk/src/edu/cmu/ri/airboat/interfaces/AirboatController.java
@@ -1,7 +1,5 @@
 package edu.cmu.ri.airboat.interfaces;
 
-import edu.cmu.ri.crw.VehicleServer;
-
 /**
  * Defines an interface for a control object that has access to the low-level
  * control and sensor interfaces to the boat.  This object will be called
@@ -20,5 +18,5 @@ public interface AirboatController {
 	 * @param sensor low-level vehicle sensor interface
 	 * @param dt elapsed time since last controller call (in seconds)
 	 */
-	public void update(VehicleServer server);
+	public void update(AirboatControl control, AirboatSensor sensor, double dt);
 }
