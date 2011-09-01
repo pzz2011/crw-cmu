34ef41073d62cf6e4c94593b89fc8c640d01f8fb
psheza <psheza@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2011-09-01 23:06:39 +0000

    resend waypoints periodically

--- apps/trunk/src/edu/cmu/ri/airboat/floodtest/BoatSimpleProxy.java
+++ apps/trunk/src/edu/cmu/ri/airboat/floodtest/BoatSimpleProxy.java
@@ -351,15 +351,25 @@ public class BoatSimpleProxy extends Thread {
         }
 
         currentWaypoint = wputm;
-        _server.startWaypoint(wputm, null, new WaypointObserver() {
-
+        _server.startWaypoint(wputm, null, new WaypointObserver() {            
+            
+            int goingCounter = 0;
+            int resendRate = 20;
             @Override
             public void waypointUpdate(WaypointState status) {
                 if (status == WaypointState.DONE) {
                     setWaypoint(_waypoints.poll());
+                } else if (status == WaypointState.GOING) {
+                    ++goingCounter;
+                    if (goingCounter >= resendRate) {
+                        setWaypoint(currentWaypoint);
+                    }
                 } else {
-                    // @todo Handle this status
-                    System.out.println("Unhandled STATUS: " + status);
+                    if (status == WaypointState.CANCELLED && goingCounter >= resendRate) {
+                        System.out.println("Waypoint was resent, old observer done");
+                    } else {
+                        System.out.println("Unhandled STATUS: " + status);
+                    }
                 }
             }
         });
