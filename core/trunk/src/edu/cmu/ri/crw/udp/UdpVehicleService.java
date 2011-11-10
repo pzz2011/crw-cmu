c34173933e2494fb16bfa2e9bf47abe6b223589a
psigen@gmail.com <psigen@gmail.com@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2011-11-10 23:00:38 +0000

    Significant updates to UDP server to begin handling serialization/deserialization

--- core/trunk/src/edu/cmu/ri/crw/udp/UdpVehicleService.java
+++ core/trunk/src/edu/cmu/ri/crw/udp/UdpVehicleService.java
@@ -32,48 +32,12 @@ public class UdpVehicleService {
     
     public static final int REGISTRATION_RATE_MS = 5000;
     public static final int PACKET_SIZE = 32767;
-
-    public enum COMMAND {
-        CMD_REGISTER_STATE_LISTENER("RSTL"),
-        CMD_SET_STATE("SS"),
-        CMD_GET_STATE("GS"),
-        CMD_REGISTER_IMAGE_LISTENER("RIL"),
-        CMD_CAPTURE_IMAGE("CI"),
-        CMD_REGISTER_CAMERA_LISTENER("CIL"),
-        CMD_START_CAMERA("STC"),
-        CMD_STOP_CAMERA("SPC"),
-        CMD_GET_CAMERA_STATUS("CS"),
-        CMD_REGISTER_SENSOR_LISTENER("RSEL"),
-        CMD_SET_SENSOR_TYPE("SST"),
-        CMD_GET_SENSOR_TYPE("GST"),
-        CMD_GET_NUM_SENSORS("GNS"),
-        CMD_REGISTER_VELOCITY_LISTENER("RVL"),
-        CMD_SET_VELOCITY("SV"),
-        CMD_GET_VELOCITY("GV"),
-        CMD_REGISTER_WAYPOINT_LISTENER("RWL"),
-        CMD_START_WAYPOINTS("STW"),
-        CMD_STOP_WAYPOINTS("SPW"),
-        CMD_GET_WAYPOINTS("GW"),
-        CMD_GET_WAYPOINT_STATUS("GWS"),
-        CMD_IS_CONNECTED("IC"),
-        CMD_IS_AUTONOMOUS("IA"),
-        CMD_SET_AUTONOMOUS("SA"),
-        CMD_SET_GAINS("SG"),
-        CMD_GET_GAINS("GG");
-
-        COMMAND(String s) {
-            str = s;
-        }
-
-        public final String str;
-    }
-    
+   
     protected VehicleServer _server;
     protected final Object _serverLock = new Object();
     
     protected final DatagramSocket _socket;
-    protected final Object _socketLock = new Object();
-
+    
     protected final List<InetSocketAddress> _registries = new ArrayList<InetSocketAddress>();
     protected final List<InetSocketAddress> _stateListeners = new ArrayList<InetSocketAddress>();
     protected final List<InetSocketAddress> _imageListeners = new ArrayList<InetSocketAddress>();
@@ -83,13 +47,11 @@ public class UdpVehicleService {
     protected final List<InetSocketAddress> _waypointListeners = new ArrayList<InetSocketAddress>();
 
     public UdpVehicleService() {
-        _server = null;
-        
         DatagramSocket s = null;
         try {
             s = new DatagramSocket();
         } catch(SocketException ex) {
-            logger.log(Level.SEVERE, "Unable to open UDP socket.", ex);
+            logger.log(Level.SEVERE, "Unable to open UDP socket", ex);
         }
         _socket = s;
     }
@@ -174,7 +136,7 @@ public class UdpVehicleService {
                     final String command = in.readUTF();
                     
                     try {
-                        switch (COMMAND.valueOf(command)) {
+                        switch (UdpConstants.COMMAND.valueOf(command)) {
                             case CMD_REGISTER_STATE_LISTENER:
                                 // TODO: registration
                                 // TODO: response
