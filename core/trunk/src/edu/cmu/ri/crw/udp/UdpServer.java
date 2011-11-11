638fd541016cd42591cd1e3f0beeb6b49b635a37
psigen <psigen@gmail.com>
2011-11-11 16:56:22 +0000

    Added request/response infrastructure, pretty close to core UDP server for three-way RPC calls.

--- core/trunk/src/edu/cmu/ri/crw/udp/UdpServer.java
+++ core/trunk/src/edu/cmu/ri/crw/udp/UdpServer.java
@@ -5,14 +5,17 @@
 
 package edu.cmu.ri.crw.udp;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketAddress;
 import java.net.SocketException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.DelayQueue;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
@@ -42,20 +45,72 @@ import java.util.concurrent.TimeUnit;
  *
  * @author Pras Velagapudi <psigen@gmail.com>
  */
-public class UdpResponder {
+public class UdpServer {
 
-    DelayQueue<Response> responses = new DelayQueue<Response>();
+    DelayQueue<QueuedResponse> responses = new DelayQueue<QueuedResponse>();
 
-    public class ResponseMaker {
-        private final ByteArrayOutputStream _buffer = new ByteArrayOutputStream(UdpConstants.INITIAL_PACKET_SIZE);
-        public final ObjectOutputStream stream = null;
+    public class Request {
+        private final ByteArrayInputStream _buffer;
+        public final DataInputStream stream;
+        public final long ticket;
+        public final SocketAddress source;
 
-        public Response toResponse() {
+        public Request(DatagramPacket packet) {
+            // Convert the packet into a data stream
+            _buffer = new ByteArrayInputStream(packet.getData());
+            stream = new DataInputStream(_buffer);
+
+            // Extract the socket address from the packet
+            source = packet.getSocketAddress();
+
+            // Extract the ticket from the data payload
+            long t = UdpConstants.NO_TICKET;
+            try {
+                t = stream.readLong();
+            } catch (IOException e) {
+                // TODO: report some sort of error here
+            }
+            ticket = t;
+        }
+
+        public void reset() {
+            _buffer.reset();
+        }
+    }
+
+    public class Response {
+        private final ByteArrayOutputStream _buffer;
+        public final DataOutputStream stream;
+        public final SocketAddress destination;
+        public final long ticket;
+
+        public Response(Request r) {
+            this(r.ticket, r.source);
+        }
+
+        public Response(long t, SocketAddress d) {
+             _buffer = new ByteArrayOutputStream(UdpConstants.INITIAL_PACKET_SIZE);
+             stream = new DataOutputStream(_buffer);
+
+             ticket = t;
+             destination = d;
+        }
+
+        public void reset() {
+            try {
+                stream.flush();
+            } catch (IOException e) {
+                // Shouldn't happen, but resetting stream anyway
+            }
+            _buffer.reset();
+        }
+
+        public QueuedResponse toQueuedResponse() {
             return null;
         }
     }
 
-    public class Response implements Delayed {
+    public class QueuedResponse implements Delayed {
         public final SocketAddress destination = null; // TODO: fix me
         public final byte[] bytes = null; // TODO: fix me
         public final int ticket = 0; // TODO: fix me
@@ -73,8 +128,8 @@ public class UdpResponder {
 
         @Override
         public int compareTo(Delayed o) {
-            if (o instanceof Response) {
-                return Long.signum(timeout - ((Response)o).timeout);
+            if (o instanceof QueuedResponse) {
+                return Long.signum(timeout - ((QueuedResponse)o).timeout);
             } else {
                 return Long.signum(getDelay(TimeUnit.NANOSECONDS) - o.getDelay(TimeUnit.NANOSECONDS));
             }
@@ -94,7 +149,7 @@ public class UdpResponder {
 
         @Override
         public void run() {
-            Response response = null;
+            QueuedResponse response = null;
 
             while(_socket.isBound() && !_socket.isClosed()) {
                 // Wait for next response that has timed out or requires transmission
@@ -130,20 +185,32 @@ public class UdpResponder {
      * Respond to an existing function request.  Prepares the response for
      * automatic retransmission and acknowledgement.
      * 
-     * @param response
+     * @param response the response to be sent
      */
     public void respond(Response response) {
-        throw new UnsupportedOperationException()
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     /**
-     * Removes all responses that have the corresponding ticket an already
+     * Send out a function response that is broadcast to a list of addresses.
+     * No retransmission will be done on these messages.  The SocketAddress
+     * specified in the Response object is not used in this case.
+     *
+     * @param response the response to be sent
+     * @param destinations the destination addresses to which it will be sent
+     */
+    public void bcast(Response response, List<SocketAddress> destinations) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    /**
+     * Removes all responses that have the corresponding ticket to an already
      * acknowledged response.
      *
      * @param ticket
      */
     public void acknowledge(long ticket) {
-        Iterator<Response> itr = responses.iterator();
+        Iterator<QueuedResponse> itr = responses.iterator();
 
         while(itr.hasNext()) {
             if (itr.next().ticket == ticket) {
@@ -151,37 +218,4 @@ public class UdpResponder {
             }
         }
     }
-
-    // Assumptions:
-    // * Server function calls are FAST w.r.t. RPC timeouts
-    // * Spurious packet loss is common
-    // * Packets may be delayed for a relatively long time (~1s) due to lossy WLAN
-    //
-    // Overview of three-way RPC from client
-    // 1. Send function call: <12131, "GET_STATE", args>
-    // 2. No response after timeout
-    // 3. Send function call: <12131, "GET_STATE", args>
-    // 4. Get response: <12131, response>
-    // 5. Send function ack: <12131, "OK">
-    //
-    // Overview of three-way RPC from server
-    // 1. Receive function call: <12131, "GET_STATE", args>
-    // 2. Execute function call on implementation
-    // 3. Send response: <12131, response>
-    // 4. No response after timeout
-    // 5. Send response: <12131, response>
-    // 6. Get function ack: <12131, "OK">
-
-    public void oneWayCall(String type, Object[] args) {
-        
-    }
-
-    public void twoWayCall(int ticket, String type, Object[] args) {
-
-    }
-
-    public void response(int ticket, Object[] args) {
-        
-    }
-    
 }
