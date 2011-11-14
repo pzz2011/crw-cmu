0417c5433757ea9c0797bf16acacee9febbe7ff4
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2011-11-14 21:15:47 +0000

    Added some boilerplate for handling channels, but still not fully implemented.

--- core/trunk/src/edu/cmu/ri/crw/udp/UdpVehicleService.java
+++ core/trunk/src/edu/cmu/ri/crw/udp/UdpVehicleService.java
@@ -2,7 +2,17 @@ package edu.cmu.ri.crw.udp;
 
 // TODO: finish this class!
 
+import edu.cmu.ri.crw.CameraListener;
+import edu.cmu.ri.crw.ImageListener;
+import edu.cmu.ri.crw.PoseListener;
+import edu.cmu.ri.crw.SensorListener;
 import edu.cmu.ri.crw.VehicleServer;
+import edu.cmu.ri.crw.VehicleServer.CameraState;
+import edu.cmu.ri.crw.VehicleServer.WaypointState;
+import edu.cmu.ri.crw.VelocityListener;
+import edu.cmu.ri.crw.WaypointListener;
+import edu.cmu.ri.crw.data.SensorData;
+import edu.cmu.ri.crw.data.Twist;
 import edu.cmu.ri.crw.data.UtmPose;
 import edu.cmu.ri.crw.udp.UdpServer.Request;
 import edu.cmu.ri.crw.udp.UdpServer.Response;
@@ -10,7 +20,9 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,13 +40,13 @@ public class UdpVehicleService implements UdpServer.RequestHandler {
     
     protected final UdpServer _udpServer = new UdpServer();
 
-    protected final List<InetSocketAddress> _registries = new ArrayList<InetSocketAddress>();
-    protected final List<InetSocketAddress> _stateListeners = new ArrayList<InetSocketAddress>();
-    protected final List<InetSocketAddress> _imageListeners = new ArrayList<InetSocketAddress>();
-    protected final List<InetSocketAddress> _cameraListeners = new ArrayList<InetSocketAddress>();
-    protected final List<InetSocketAddress> _sensorListeners = new ArrayList<InetSocketAddress>();
-    protected final List<InetSocketAddress> _velocityListeners = new ArrayList<InetSocketAddress>();
-    protected final List<InetSocketAddress> _waypointListeners = new ArrayList<InetSocketAddress>();
+    protected final List<SocketAddress> _registries = new ArrayList<SocketAddress>();
+    protected final Map<SocketAddress, Integer> _stateListeners = new LinkedHashMap<SocketAddress, Integer>();
+    protected final Map<SocketAddress, Integer> _imageListeners = new LinkedHashMap<SocketAddress, Integer>();
+    protected final Map<SocketAddress, Integer> _cameraListeners = new LinkedHashMap<SocketAddress, Integer>();
+    protected final ArrayList<Map<SocketAddress,Integer>> _sensorListeners = new ArrayList<Map<SocketAddress, Integer>>();
+    protected final Map<SocketAddress, Integer> _velocityListeners = new LinkedHashMap<SocketAddress, Integer>();
+    protected final Map<SocketAddress, Integer> _waypointListeners = new LinkedHashMap<SocketAddress, Integer>();
 
     public UdpVehicleService() {
         _udpServer.setHandler(this);
@@ -93,8 +105,9 @@ public class UdpVehicleService implements UdpServer.RequestHandler {
 
             switch (UdpConstants.COMMAND.fromStr(command)) {
                 case CMD_REGISTER_STATE_LISTENER:
-                    // TODO: registration
-                    // TODO: response
+                    synchronized(_stateListeners) {
+                        _stateListeners.put(req.source, UdpConstants.REGISTRATION_TIMEOUT_COUNT);
+                    }
                     break;
                 case CMD_SET_STATE:
                     _vehicleServer.setPose(UdpConstants.readPose(req.stream));
@@ -105,8 +118,9 @@ public class UdpVehicleService implements UdpServer.RequestHandler {
                     _udpServer.respond(resp);
                     break;
                 case CMD_REGISTER_IMAGE_LISTENER:
-                    // TODO: registration
-                    // TODO: response
+                    synchronized(_imageListeners) {
+                        _imageListeners.put(req.source, UdpConstants.REGISTRATION_TIMEOUT_COUNT);
+                    }
                     break;
                 case CMD_CAPTURE_IMAGE:
                     byte[] image = _vehicleServer.captureImage(req.stream.readInt(), req.stream.readInt());
@@ -115,8 +129,9 @@ public class UdpVehicleService implements UdpServer.RequestHandler {
                     _udpServer.respond(resp);
                     break;
                 case CMD_REGISTER_CAMERA_LISTENER:
-                    // TODO: registration
-                    // TODO: response
+                    synchronized(_cameraListeners) {
+                        _cameraListeners.put(req.source, UdpConstants.REGISTRATION_TIMEOUT_COUNT);
+                    }
                     break;
                 case CMD_START_CAMERA:
                     _vehicleServer.startCamera(
@@ -133,8 +148,24 @@ public class UdpVehicleService implements UdpServer.RequestHandler {
                     _udpServer.respond(resp); 
                     break;
                 case CMD_REGISTER_SENSOR_LISTENER:
-                    // TODO: registration
-                    // TODO: response
+                    synchronized(_sensorListeners) {
+                        int channel = req.stream.readInt();
+                        
+                        // Make sure the sensorListener array is large enough
+                        _sensorListeners.ensureCapacity(channel+1);
+                        while (_sensorListeners.size() < channel+1)
+                            _sensorListeners.add(null);
+                        
+                        // Retrive the sensor sublisteners
+                        Map<SocketAddress, Integer> _listeners = _sensorListeners.get(channel);
+                        if (_listeners == null) {
+                            _listeners = new LinkedHashMap<SocketAddress, Integer>();
+                            _sensorListeners.set(channel, _listeners);
+                        }
+                        
+                        // Add the address to the appropriate sublistener list
+                        _listeners.put(req.source, UdpConstants.REGISTRATION_TIMEOUT_COUNT);
+                    }
                     break;
                 case CMD_SET_SENSOR_TYPE:
                     _vehicleServer.setSensorType(req.stream.readInt(), 
@@ -150,8 +181,9 @@ public class UdpVehicleService implements UdpServer.RequestHandler {
                     _udpServer.respond(resp);
                     break;
                 case CMD_REGISTER_VELOCITY_LISTENER:
-                    // TODO: registration
-                    // TODO: response
+                    synchronized(_velocityListeners) {
+                        _velocityListeners.put(req.source, UdpConstants.REGISTRATION_TIMEOUT_COUNT);
+                    }
                     break;
                 case CMD_SET_VELOCITY:
                     _vehicleServer.setVelocity(UdpConstants.readTwist(req.stream));
@@ -162,8 +194,9 @@ public class UdpVehicleService implements UdpServer.RequestHandler {
                     _udpServer.respond(resp);
                     break;
                 case CMD_REGISTER_WAYPOINT_LISTENER:
-                    // TODO: registration
-                    // TODO: response
+                    synchronized(_waypointListeners) {
+                        _waypointListeners.put(req.source, UdpConstants.REGISTRATION_TIMEOUT_COUNT);
+                    }
                     break;
                 case CMD_START_WAYPOINTS:
                     UtmPose[] swPoses = new UtmPose[req.stream.readInt()];
@@ -237,5 +270,108 @@ public class UdpVehicleService implements UdpServer.RequestHandler {
      */
     public void shutdown() {
         _udpServer.stop();
+    }
+    
+    private final StreamHandler _handler = new StreamHandler();
+    
+    private class StreamHandler implements PoseListener, ImageListener, CameraListener, SensorListener, VelocityListener, WaypointListener {
+
+        @Override
+        public void receivedPose(UtmPose pose) {
+            try {
+                // Construct pose message
+                Response resp = new Response(UdpConstants.NO_TICKET, null);
+                resp.stream.writeUTF(UdpConstants.COMMAND.CMD_SEND_STATE.str);
+                UdpConstants.writePose(resp.stream, pose);
+            
+                // Send to all listeners
+                _udpServer.bcast(resp, _stateListeners.keySet());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to serialize pose");
+            }
+        }
+
+        @Override
+        public void receivedImage(byte[] image) {
+            try {
+                // Construct pose message
+                Response resp = new Response(UdpConstants.NO_TICKET, null);
+                resp.stream.writeUTF(UdpConstants.COMMAND.CMD_SEND_IMAGE.str);
+                resp.stream.writeInt(image.length);
+                resp.stream.write(image);
+            
+                // Send to all listeners
+                _udpServer.bcast(resp, _imageListeners.keySet());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to serialize image");
+            }
+        }
+
+        @Override
+        public void imagingUpdate(CameraState status) {
+            try {
+                // Construct pose message
+                Response resp = new Response(UdpConstants.NO_TICKET, null);
+                resp.stream.writeUTF(UdpConstants.COMMAND.CMD_SEND_CAMERA.str);
+                resp.stream.writeByte(status.ordinal());
+            
+                // Send to all listeners
+                _udpServer.bcast(resp, _cameraListeners.keySet());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to serialize camera");
+            }
+        }
+
+        @Override
+        public void receivedSensor(SensorData sensor) {
+            try {
+                // Construct pose message
+                Response resp = new Response(UdpConstants.NO_TICKET, null);
+                resp.stream.writeUTF(UdpConstants.COMMAND.CMD_SEND_SENSOR.str);
+                resp.stream.writeInt(sensor.channel);
+                resp.stream.writeByte(sensor.type.ordinal());
+                resp.stream.writeInt(sensor.data.length);
+                for (int i = 0; i < sensor.data.length; ++i)
+                    resp.stream.writeDouble(sensor.data[i]);
+            
+                // Send to all listeners
+                Map<SocketAddress, Integer> _listeners = _sensorListeners.get(sensor.channel);
+                if (_listeners != null)
+                    _udpServer.bcast(resp, _listeners.keySet());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to serialize sensor " + sensor.channel);
+            }
+        }
+
+        @Override
+        public void receivedVelocity(Twist velocity) {
+            try {
+                // Construct pose message
+                Response resp = new Response(UdpConstants.NO_TICKET, null);
+                resp.stream.writeUTF(UdpConstants.COMMAND.CMD_SEND_VELOCITY.str);
+                UdpConstants.writeTwist(resp.stream, velocity);
+            
+                // Send to all listeners
+                _udpServer.bcast(resp, _velocityListeners.keySet());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to serialize camera");
+            }
+        }
+
+        @Override
+        public void waypointUpdate(WaypointState status) {
+            try {
+                // Construct pose message
+                Response resp = new Response(UdpConstants.NO_TICKET, null);
+                resp.stream.writeUTF(UdpConstants.COMMAND.CMD_SEND_WAYPOINT.str);
+                resp.stream.writeByte(status.ordinal());
+            
+                // Send to all listeners
+                _udpServer.bcast(resp, _waypointListeners.keySet());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to serialize camera");
+            }
+        }
+        
     }
 }
