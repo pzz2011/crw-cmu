950aceb723121a644c8c0cdd24bc28665da362db
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2011-11-14 14:47:10 +0000

    Added toString methods for data types (easier debugging)

--- core/trunk/src/edu/cmu/ri/crw/data/SensorData.java
+++ core/trunk/src/edu/cmu/ri/crw/data/SensorData.java
@@ -2,6 +2,7 @@ package edu.cmu.ri.crw.data;
 
 import edu.cmu.ri.crw.VehicleServer.SensorType;
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * Structure for holding sensor data.
@@ -12,4 +13,9 @@ public class SensorData implements Cloneable, Serializable  {
     public int channel;
     public double[] data;
     public SensorType type;
+    
+    @Override
+    public String toString() {
+        return "Sensor" + channel + "(" + type + ")" + Arrays.toString(data);
+    }
 }
