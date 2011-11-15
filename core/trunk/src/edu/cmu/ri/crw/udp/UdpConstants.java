a231a6bd33ad82e98f0a337f98cce5245039d8d8
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2011-11-15 17:22:15 +0000

    Fixed size limit that was interfering with image transfer.

--- core/trunk/src/edu/cmu/ri/crw/udp/UdpConstants.java
+++ core/trunk/src/edu/cmu/ri/crw/udp/UdpConstants.java
@@ -27,7 +27,8 @@ public class UdpConstants {
     public static final long TIMEOUT_NS = (RETRY_COUNT + 1) * RETRY_RATE_NS;
     public static final int NO_TICKET = -1;
 
-    public static final int MAX_PACKET_SIZE = 512;
+    public static final int INITIAL_PACKET_SIZE = 512;
+    public static final int MAX_PACKET_SIZE = 16777216;
     public static final String CMD_ACKNOWLEDGE = "OK";
 
     /**
