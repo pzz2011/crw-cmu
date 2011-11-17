12c3a859d456b3bb3c21518f76c7c9b1dd8ff9ce
psigen <psigen@gmail.com>
2011-11-17 04:39:33 +0000

    Replaced controller with multi-waypoint updated version derived from SimpleBoatController.

--- server/trunk/src/edu/cmu/ri/airboat/server/AirboatController.java
+++ server/trunk/src/edu/cmu/ri/airboat/server/AirboatController.java
@@ -1,7 +1,6 @@
 package edu.cmu.ri.airboat.server;
 
 import robotutils.Pose3D;
-import android.util.Log;
 import edu.cmu.ri.crw.VehicleController;
 import edu.cmu.ri.crw.VehicleServer;
 import edu.cmu.ri.crw.data.Twist;
@@ -16,61 +15,62 @@ import edu.cmu.ri.crw.data.UtmPose;
  * 
  */
 public enum AirboatController {
-	
+
 	/**
 	 * This controller turns the boat around until it is facing the waypoint,
 	 * then drives roughly in an arc towards the waypoint. When it gets within a
 	 * certain range, it will cut power to the boat entirely.
 	 */
-
 	POINT_AND_SHOOT(new VehicleController() {
-		private final String logTag = AirboatController.class.getName();
 
 		@Override
 		public void update(VehicleServer server, double dt) {
 			Twist twist = new Twist();
 
-			// Get the position of the vehicle and the waypoint
-			UtmPose utmPose = server.getPose();
-			UtmPose waypointPose = server.getWaypoints()[0];
-			if (utmPose == null || waypointPose == null) {
-				Log.w(logTag, "State or waypoint was null, not updating.");
+			// Get the position of the vehicle
+			UtmPose state = server.getPose();
+			Pose3D pose = state.pose;
+
+			// Get the current waypoint, or return if there are none
+			UtmPose[] waypoints = server.getWaypoints();
+			if (waypoints == null || waypoints.length <= 0) {
+				server.setVelocity(twist);
 				return;
 			}
-			
-			Pose3D pose = utmPose.pose;
-			Pose3D waypoint = waypointPose.pose;
+			Pose3D waypoint = waypoints[0].pose;
 
 			// TODO: handle different UTM zones!
 
 			// Compute the distance and angle to the waypoint
-			// TODO: compute distance more efficiently
-			double distance = Math.sqrt(Math.pow(
-					(waypoint.getX() - pose.getX()), 2)
-					+ Math.pow((waypoint.getY() - pose.getY()), 2));
-			double angle = Math.atan2((waypoint.getY() - pose.getY()),
-					(waypoint.getX() - pose.getX()))
+			double distanceSq = planarDistanceSq(pose, waypoint);
+			double angle = angleBetween(pose, waypoint)
 					- pose.getRotation().toYaw();
 			angle = normalizeAngle(angle);
 
-			// Choose driving behavior depending on direction and and where we
-			// are
+			// Choose driving behavior depending on direction and where we are
 			if (Math.abs(angle) > 1.0) {
 
 				// If we are facing away, turn around first
 				twist.dx(0.5);
 				twist.drz(Math.max(Math.min(angle / 1.0, 1.0), -1.0));
-			} else if (distance >= 3.0) {
+			} else if (distanceSq >= 9.0) {
 
 				// If we are far away, drive forward and turn
-				twist.dx(Math.min(distance / 10.0, 1.0));
-				twist.drz(Math.max(Math.min(angle / 10.0, 1.0), -1.0));
+				twist.dx(Math.min(distanceSq / 10.0, 1.0));
+				twist.drz(Math.max(Math.min(angle / 5.0, 1.0), -1.0));
+			} else {
+
+				// If we are "at" the destination, de-queue a waypoint
+				UtmPose[] queuedWaypoints = new UtmPose[waypoints.length - 1];
+				System.arraycopy(waypoints, 1, queuedWaypoints, 0,
+						queuedWaypoints.length);
+				server.startWaypoints(queuedWaypoints,
+						AirboatController.POINT_AND_SHOOT.toString());
 			}
 
 			// Set the desired velocity
 			server.setVelocity(twist);
 		}
-
 	}),
 
 	/**
@@ -79,7 +79,6 @@ public enum AirboatController {
 	 * way, and completely ignores the waypoint.
 	 */
 	STOP(new VehicleController() {
-
 		@Override
 		public void update(VehicleServer server, double dt) {
 			server.setVelocity(new Twist());
@@ -91,39 +90,40 @@ public enum AirboatController {
 	 * This controller shoots a picture only if it moves to a significantly
 	 * different pose
 	 */
-
 	SHOOT_ON_MOVE(new VehicleController() {
 		UtmPose lastPose = new UtmPose();
-		
+
 		@Override
 		public void update(VehicleServer server, double dt) {
 			Twist twist = new Twist();
-			// Get the position of the vehicle and the waypoint
-			UtmPose utmPose = server.getPose();
-			Pose3D pose = utmPose.pose;
-			UtmPose waypointState = server.getWaypoints()[0];
-			Pose3D waypoint = waypointState.pose;
+
+			// Get the position of the vehicle
+			UtmPose state = server.getPose();
+			Pose3D pose = state.pose;
+
+			// Get the current waypoint, or return if there are none
+			UtmPose[] waypoints = server.getWaypoints();
+			if (waypoints == null || waypoints.length <= 0) {
+				server.setVelocity(twist);
+				return;
+			}
+			Pose3D waypoint = waypoints[0].pose;
 
 			// TODO: handle different UTM zones!
 			// Compute the distance and angle to the waypoint
-			// TODO: compute distance more efficiently
-			double distance = Math.sqrt(Math.pow(
-					(waypoint.getX() - pose.getX()), 2)
-					+ Math.pow((waypoint.getY() - pose.getY()), 2));
-			double angle = Math.atan2((waypoint.getY() - pose.getY()),
-					(waypoint.getX() - pose.getX()))
+			double distanceSq = planarDistanceSq(pose, waypoint);
+			double angle = angleBetween(pose, waypoint)
 					- pose.getRotation().toYaw();
 			angle = normalizeAngle(angle);
 
-			// Choose driving behavior depending on direction and and where we
-			// are
+			// Choose driving behavior depending on direction and where we are
 			if (Math.abs(angle) > 1.0) {
 				// If we are facing away, turn around first
 				twist.dx(0.5);
 				twist.drz(Math.max(Math.min(angle / 1.0, 1.0), -1.0));
-			} else if (distance >= 3.0) {
+			} else if (distanceSq >= 9.0) {
 				// If we are far away, drive forward and turn
-				twist.dx(Math.min(distance / 10.0, 1.0));
+				twist.dx(Math.min(distanceSq / 10.0, 1.0));
 				twist.drz(Math.max(Math.min(angle / 10.0, 1.0), -1.0));
 			}
 
@@ -136,12 +136,15 @@ public enum AirboatController {
 					|| isNovel(pose, waypoint, PlanningMethod.SIMPLE) > 0.8) {
 				lastPose.pose = pose.clone();
 				System.out.println("Should Capture now!!!!!");
-				server.startCamera(1, 0, 640, 480);
+				server.startCamera(1, 0.0, 640, 480);
 			}
+
+			// TODO: add next waypoint functionality back in!!
 		}
 
 		/**
-		 * Takes a pose and determines whether the image to be taken is novel or not
+		 * Takes a pose and determines whether the image to be taken is novel or
+		 * not
 		 * 
 		 * @param pose
 		 *            The current pose
@@ -153,12 +156,16 @@ public enum AirboatController {
 		 */
 		double isNovel(Pose3D pose, Pose3D waypoint, PlanningMethod method) {
 
-			// Assuming that the angle of view of the camera is 30 Degrees
-			final double CAMERA_AOV = Math.PI / 180.0f * 30;
+			final double CAMERA_AOV = Math.PI / 180.0f * 30; // Assuming that
+																// the angle of
+																// view of the
+																// camera is 30
+																// Degrees
 			final double OVERLAP_RATIO = 0.8f;
-			// The effective distance till which the camera resolution/detection is
-			// trusted
-			final double EFFECTIVE_DISTANCE = 10.0;
+			final double EFFECTIVE_DISTANCE = 10.0; // The effective distance
+													// till which the camera
+													// resolution/detection is
+													// trusted
 			double novelty = 0.0;
 			switch (method) {
 			case SIMPLE:
@@ -177,31 +184,32 @@ public enum AirboatController {
 								+ (lastPose.pose.getY() - pose.getY())
 								* (lastPose.pose.getY() - pose.getY()));
 
-				// Assign roughly half initial weight to yaw, and half to distance
-
+				// Assign half weight to yaw, and half to distance
 				if (angle >= CAMERA_AOV * OVERLAP_RATIO) {
-					// i.e. if the current yaw has changed more than the previous
-					// orientation by greater than 30 degrees * overlap factor
+					// i.e. if the current yaw has changed more than the
+					// previous orientation by greater than 30 degrees * overlap
+					// factor
+
+					// This is because ANY yaw greater than the angle of view
+					// will have completely new info (Think sectors)
 					novelty = 0.5 * angle / (CAMERA_AOV * OVERLAP_RATIO);
-					// This is because ANY yaw greater than the angle of view will
-					// have completely new info (Think sectors)
-					// Assuming that the zone of overlap is not useful information
+
+					// Assuming that the zone of overlap is not useful
+					// information
 				}
 
 				novelty += (distance / EFFECTIVE_DISTANCE) * 0.5;
 
 				break;
-			/**
-			 * This planning method would involve a grid based image capture routine
-			 */
 			case GEOMETRIC:
 				throw new UnsupportedOperationException("Not implemented Yet!");
 
 			}
 			return novelty;
 		}
+
 	});
-	
+
 	public enum PlanningMethod {
 		SIMPLE, GEOMETRIC
 	};
@@ -214,7 +222,8 @@ public enum AirboatController {
 	/**
 	 * Instantiates a library entry with the specified controller.
 	 * 
-	 * @param controller the controller to be used by this entry.
+	 * @param controller
+	 *            the controller to be used by this entry.
 	 */
 	private AirboatController(VehicleController controller) {
 		this.controller = controller;
@@ -223,7 +232,8 @@ public enum AirboatController {
 	/**
 	 * Takes an angle and shifts it to be in the range -Pi to Pi.
 	 * 
-	 * @param angle an angle in radians
+	 * @param angle
+	 *            an angle in radians
 	 * @return the same angle as given, normalized to the range -Pi to Pi.
 	 */
 	public static double normalizeAngle(double angle) {
@@ -232,5 +242,39 @@ public enum AirboatController {
 		while (angle < -Math.PI)
 			angle += 2 * Math.PI;
 		return angle;
+	}
+
+	/**
+	 * Computes the squared XY-planar Euclidean distance between two points.
+	 * Using the squared distance is cheaper (it avoid a sqrt), and for constant
+	 * comparisons, it makes no difference (just square the constant).
+	 * 
+	 * @param a
+	 *            the first pose
+	 * @param b
+	 *            the second pose
+	 * @return the XY-planar Euclidean distance
+	 */
+	public static double planarDistanceSq(Pose3D a, Pose3D b) {
+		double dx = a.getX() - b.getX();
+		double dy = a.getY() - b.getY();
+		return dx * dx + dy * dy;
+	}
+
+	/**
+	 * Computes a direction vector from a source pose to a destination pose, as
+	 * projected onto the XY-plane. Returns an angle representing the direction
+	 * in the XY-plane to take if starting at the source pose to reach the
+	 * destination pose.
+	 * 
+	 * @param a
+	 *            the source (starting) pose
+	 * @param b
+	 *            the destination (final) pose
+	 * @return an angle in the XY-plane (around +Z-axis) to get to destination
+	 */
+	public static double angleBetween(Pose3D src, Pose3D dest) {
+		return Math.atan2((dest.getY() - src.getY()),
+				(dest.getX() - src.getX()));
 	}
 }
