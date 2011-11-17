0f1255d5079f541181c504deb279d78dd2fad6a1
psheza <psheza@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2011-11-17 23:17:07 +0000

    Initial code for PoP demo

---
+++ apps/trunk/src/edu/cmu/ri/airboat/floodtest/BufferedImageWithPose.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cmu.ri.airboat.floodtest;
+
+import edu.cmu.ri.crw.data.UtmPose;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author pscerri
+ */
+public class BufferedImageWithPose implements Comparable<BufferedImageWithPose> {
+    
+    final BufferedImage img;
+    private final UtmPose pose;
+    long c = ImagePanel.count++;
+
+    public BufferedImageWithPose(BufferedImage img, UtmPose pose) {
+        this.img = img;
+        this.pose = pose;
+    }
+
+    public int compareTo(BufferedImageWithPose t) {
+        if (t.c > c) {
+            return 1;
+        } else if (t.c < c) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public BufferedImage getImg() {
+        return img;
+    }
+
+    public UtmPose getPose() {
+        return pose;
+    }
+    
+    
+}
