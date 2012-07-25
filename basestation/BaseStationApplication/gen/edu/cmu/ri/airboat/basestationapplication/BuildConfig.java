02ba76b3d31b155b8b62ea4390d3b2fe17bc0eee
psigen <psigen@gmail.com>
2012-07-25 19:03:43 +0000

    Removed all the genrerated subdirectories.

--- basestation/BaseStationApplication/gen/edu/cmu/ri/airboat/basestationapplication/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.cmu.ri.airboat.basestationapplication;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
