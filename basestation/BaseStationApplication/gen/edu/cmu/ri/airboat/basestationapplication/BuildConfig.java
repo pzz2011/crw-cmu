10260aeef99c670046a660e9d731596adf8dfc16
osman.khwaja <osman.khwaja@gmail.com>
2012-07-25 17:10:04 +0000

    First version of Hashim's Base station application

---
+++ basestation/BaseStationApplication/gen/edu/cmu/ri/airboat/basestationapplication/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.cmu.ri.airboat.basestationapplication;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
