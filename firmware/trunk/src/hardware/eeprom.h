26fbebfaf7a3a41d3547137651b7343b950d0923
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2012-12-21 05:46:32 +0000

    Added port of eeprom reads and writes, i think.

--- firmware/trunk/src/hardware/eeprom.h
+++ firmware/trunk/src/hardware/eeprom.h
@@ -4,22 +4,22 @@
  * Contains a few helper functions for loading and unloading data
  * from persistent EEPROM memory.
  */
-#include <WProgram.h>
+#include <avr/eeprom.h>
 
 template <class T> int EEPROM_writeAnything(int ee, const T& value)
 {
-    const byte* p = (const byte*)(const void*)&value;
+    const char* p = (const char*)(const void*)&value;
     int i;
     for (i = 0; i < sizeof(value); i++)
-	  EEPROM.write(ee++, *p++);
+	  eeprom_write_byte(ee++, *p++);
     return i;
 }
 
 template <class T> int EEPROM_readAnything(int ee, T& value)
 {
-    byte* p = (byte*)(void*)&value;
+    char* p = (char*)(void*)&value;
     int i;
     for (i = 0; i < sizeof(value); i++)
-	  *p++ = EEPROM.read(ee++);
+	  *p++ = eeprom_read_byte(ee++);
     return i;
 }
