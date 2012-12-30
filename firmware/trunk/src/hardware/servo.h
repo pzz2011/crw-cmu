9971c464f79f104b930b3d312a19107e098f3f0e
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2012-12-30 20:32:04 +0000

    Fixed up the task timer, it should work now (roughly).

--- firmware/trunk/src/hardware/servo.h
+++ firmware/trunk/src/hardware/servo.h
@@ -20,7 +20,7 @@
 #define MAX_PULSE_WIDTH_US      2400L    // the longest pulse sent to a servo 
 #define DEFAULT_PULSE_WIDTH_US  1500L    // default pulse width when servo is attached
 #define REFRESH_INTERVAL_US    20000L    // minimum time to refresh servos
-#define SCALE_FACTOR           (65535L / (MAX_PULSE_WIDTH_US - MIN_PULSE_WIDTH_US))
+#define SERVO_SCALE_FACTOR    (65535L / (MAX_PULSE_WIDTH_US - MIN_PULSE_WIDTH_US))
 
 struct ServoConfig
 {
@@ -34,13 +34,6 @@ class Servo
 {
  public:
   Servo() {
-    // Set up the timer to a 0.5MHz tick resolution, so we
-    // can convert timings easily (1 tick = 2uS)
-    _config.timer.CNT = 0; // Start the counter at 0
-    _config.timer.PER = REFRESH_INTERVAL_US >> 1; // Set the PWM resolution
-    _config.timer.CTRLA = TC_CLKSEL_DIV64_gc; // 32MHz / 64 = 0.5Mhz
-    _config.timer.CTRLB = TC0_CCAEN_bm | TC_WGMODE_SS_gc; 
-
     // Initialize the timer to the default position
     _position = DEFAULT_PULSE_WIDTH_US;
     update(_position);
@@ -48,26 +41,35 @@ class Servo
     // Set up the timer pin as an output
     _config.port.OUTCLR = _BV(_config.pin);
     _config.port.DIRSET = _BV(_config.pin);
+
+    // Set up the timer to a 0.5MHz tick resolution, so we
+    // can convert timings easily (1 tick = 2uS)
+    _config.timer.CNT = 0; // Start the counter at 0
+    _config.timer.PER = REFRESH_INTERVAL_US >> 1; // Set the PWM resolution
+    _config.timer.CTRLB = TC0_CCAEN_bm | TC_WGMODE_SS_gc; 
+
+    // Start the timer running
+    _config.timer.CTRLA = TC_CLKSEL_DIV64_gc; // 32MHz / 64 = 0.5Mhz
   }
 
   ~Servo() {
+    // Disable the timer
+    _config.timer.CTRLA = 0;
+
     // Set up the timer pin as an input
     _config.port.OUTCLR = _BV(_config.pin);
     _config.port.DIRCLR = _BV(_config.pin);
-
-    // Disable the timer
-    _config.timer.CTRLA = 0;
   }
 
   void set(uint16_t position)
   {
-    _position = position / SCALE_FACTOR;
+    _position = position / SERVO_SCALE_FACTOR;
     update(_position);
   }
 
   int get()
   {
-    return _position * SCALE_FACTOR;
+    return _position * SERVO_SCALE_FACTOR;
   }
 
   void setRaw(uint16_t position)
