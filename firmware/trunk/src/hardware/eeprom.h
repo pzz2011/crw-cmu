cc9304314e5c1cba6c5ca95e9729957f307a9a6b
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2012-12-31 19:07:38 +0000

    Fixed errors in eeprom read/write.

--- firmware/trunk/src/hardware/eeprom.h
+++ firmware/trunk/src/hardware/eeprom.h
@@ -6,18 +6,12 @@
  */
 #include <avr/eeprom.h>
 
-template <class T> int eeprom_write(int ee, const T& value) {
-    const char* p = (const char*) (const void*) &value;
-    uint8_t i;
-    for (i = 0; i < sizeof (value); i++)
-        eeprom_write_byte(*p++, ee++);
-    return i;
+template <class T> void eeprom_write(int ee, const T& value) {
+    const void* p = (const void*) &value;
+    eeprom_write_block(p, (void*)ee, sizeof(value));
 }
 
-template <class T> int eeprom_read(int ee, T& value) {
-    char* p = (char*) (void*) &value;
-    uint8_t i;
-    for (i = 0; i < sizeof (value); i++)
-        eeprom_read_byte(*p++, ee++);
-    return i;
+template <class T> void eeprom_read(int ee, T& value) {
+    void* p = (void*) &value;
+    eeprom_read_block(p, (void*)ee, sizeof(value));
 }
