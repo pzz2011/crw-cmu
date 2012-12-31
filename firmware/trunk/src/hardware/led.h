fe2222ddf77f632de17ae03d45c710bee5bdeb57
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2012-12-31 00:22:13 +0000

    Successful LED test at 32Mhz clock rate.

--- firmware/trunk/src/hardware/led.h
+++ firmware/trunk/src/hardware/led.h
@@ -11,32 +11,32 @@
 #include <avr/io.h>
 
 struct LedConfig {
-  PORT_t port;
+  PORT_t *port;
   int outputPin;
   bool isActiveHigh;
 };
 
-template<LedConfig &_led>
+template<const LedConfig &_led>
 class Led {
  public:
   Led(void) { 
 
     // Turn the LED off by default
     if (_led.isActiveHigh) {
-      _led.port.OUTCLR = _BV(_led.outputPin);
+      _led.port->OUTCLR = _BV(_led.outputPin);
     } else {
-      _led.port.OUTSET = _BV(_led.outputPin);
+      _led.port->OUTSET = _BV(_led.outputPin);
     }
 
     // Set the LED port output direction
-    _led.port.DIRSET = _BV(_led.outputPin);
+    _led.port->DIRSET = _BV(_led.outputPin);
   }
 
   ~Led(void) {
 
     // Set the LED pin to be an input and low
-    _led.port.OUTCLR = _BV(_led.outputPin);
-    _led.port.DIRCLR = _BV(_led.outputPin);
+    _led.port->OUTCLR = _BV(_led.outputPin);
+    _led.port->DIRCLR = _BV(_led.outputPin);
   }
 
   void on(void)
@@ -54,9 +54,9 @@ class Led {
     // Turn the LED on or off as specified
     // (invert the logic when active low)
     if (_led.isActiveHigh ^ enabled) {
-      _led.port.OUTCLR = _BV(_led.outputPin);
+      _led.port->OUTCLR = _BV(_led.outputPin);
     } else {
-      _led.port.OUTSET = _BV(_led.outputPin);
+      _led.port->OUTSET = _BV(_led.outputPin);
     }
   }
 };
