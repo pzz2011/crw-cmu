be75dde77ead3fa58a2bce3982cf993da239c864
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2013-01-01 22:35:30 +0000

    Restructured sensors to use the template system.  It's not ideal, but it will have to do for now.

--- firmware/trunk/src/sensors/do_sensor.h
+++ firmware/trunk/src/sensors/do_sensor.h
@@ -1,1 +1,46 @@
-#include <string>
+#ifndef DO_SENSOR_H
+#define DO_SENSOR_H
+
+#include "serial.h"
+#include <string.h>
+
+#define RECV_DO_FN 'o'
+
+template<const SerialConfig &_config>
+class DOSensor
+{
+  char doReading[5];
+  std::string sensorstring = "";
+  
+  Serial<_config> serial;
+  FILE *stream;
+  MeetAndroid *amarino;
+
+ DOSensor(MeetAndroid *a) : serial(BAUD_38400) {  
+    stream = serial.stream();
+    amarino = a;
+
+    // The command "C" will tell the stamp to take continues readings
+    fputs("\rC\r", stream);
+  }
+
+  ~DOSensor() { }
+
+  void update() {
+    while (serial.available()) {       
+      char inchar = fgetc(stream);  // Get the char we just received
+      sensorstring += inchar;       // Add it to the inputString
+      
+      if (inchar == '\r') {         // if a string from the Atlas Scientific product has been received in its entirety
+	sensorstring.toCharArray(doReading, 5);
+	amarino->send(RECV_DO_FN);
+	amarino->send(doReading);
+	amarino->sendln();
+	sensorstring = "";
+	break;
+      }
+    } 
+  }
+}
+
+#endif /* DO_SENSOR_H */
