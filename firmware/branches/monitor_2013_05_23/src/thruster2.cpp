3edf0524cf9e228bd005bb58c39d6612316f983b
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2013-05-25 16:08:11 +0000

    Committed faster current limit and new PID gains.

--- firmware/branches/monitor_2013_05_23/src/thruster2.cpp
+++ firmware/branches/monitor_2013_05_23/src/thruster2.cpp
@@ -14,23 +14,23 @@ void Thruster2::arm(void)
 {
   servo->set1(32000);
   servo->set2(32000);
-  _delay_ms(10000);
-
-  servo->set1(-32000);
-  servo->set2(-32000);
-  _delay_ms(10000);
+  _delay_ms(3000);
 
   servo->set1(0);
   servo->set2(0);
-  _delay_ms(10000);
+  _delay_ms(5000);
+
+  servo->set1(0);
+  servo->set2(0);
+  _delay_ms(2000);
 }
 
 void Thruster2::update(void)
 {
   float output1, output2;
 
-  output1 = (desiredVelocity[0] + desiredVelocity[5]) * 10000;
-  output2 = (desiredVelocity[0] - desiredVelocity[5]) * 10000;
+  output1 = (desiredVelocity[0] + desiredVelocity[5]) * 20000;
+  output2 = (desiredVelocity[0] - desiredVelocity[5]) * 20000;
 
   if (output1 < TMIN)
     output1 = TMIN;
