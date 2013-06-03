bd5d5270dfff1f23622810a7fce170ea9176c81b
psigen <psigen@b8193c68-4c09-43fb-e0d1-26853b6b58d3>
2013-06-03 20:46:33 +0000

    Took out debugging print statement and added fix to use thrust PIDs in controller.

--- server/trunk/src/edu/cmu/ri/airboat/server/AirboatController.java
+++ server/trunk/src/edu/cmu/ri/airboat/server/AirboatController.java
@@ -111,16 +111,19 @@ public enum AirboatController {
 					pos = 1.0;
 				
 				// THRUST CONTROL SEGMENT
-				double thrust = AirboatImpl.CONST_THRUST;
+				double[] thrust_pids = server_impl.getGains(0);
+				double thrust = AirboatImpl.CONST_THRUST * thrust_pids[0];
 				
 				// update twist
 				twist.dx(thrust);
 				twist.drz(pos);
-				// log relevant variables 
+				// log relevant variables
+				/*
 				logger_osman.info("Waypoint: " + waypoint);
 				logger_osman.info("DEBUG: " + distanceSq + " " + angle_destination + " " + angle_boat + " " + drz + " " +
 						prev_angle_destination + " " + angle_destination_change + " " + pos + " " + thrust
 						+ " " + bSum + " " + rudder_pids[0] + " " + rudder_pids[1] + " " + rudder_pids[2]);
+				*/
 				// update angle error
 				prev_angle_destination = angle_destination;
 				// Set the desired velocity
